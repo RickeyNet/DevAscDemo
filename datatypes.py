df4fb861aa1222d9d7e5ef0a83471ba40b5a72cb
Rickey <accode@protonmail.com>
2025-10-22 16:40:51 -0500

    Methods

--- datatypes.py
+++ datatypes.py
@@ -1,7 +1,4 @@
-from classes.my_classes import Car
+my_string = "hello, World!"
 
-my_car = Car(make="Jeep", model="wagoneer", year=2024, mileage=3000, color="Nordic Grey", condition="New")
-
-my_wifes_car = Car(make="Toyota", model="Camry", year=2020, mileage=15000, color="Blue", condition="Used")
-
-print(my_wifes_car.__dict__)
+print(my_string.strip().capitalize())
+print(my_integer.)
